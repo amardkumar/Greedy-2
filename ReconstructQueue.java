519ef52d955415b3448cc9ef68b04a13dc4bd984
amardkumar <102793539+amardkumar@users.noreply.github.com>
2025-07-25 16:05:09 -0400

    Queue Reconstruction by Height Implementation

---
+++ ReconstructQueue.java
@@ -0,0 +1,23 @@
+//Time Complexity: O(nlogn)
+//Space Complexity: O(n)
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        List<int []> result = new ArrayList<>();
+        Arrays.sort(people, (a,b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }
+            return b[0] - a[0];
+        });
+
+        for(int [] person : people){
+            result.add(person[1], person);
+        }
+
+        int[][] res = new int[people.length][2];
+        for(int i=0; i<res.length;i++){
+            res[i] = result.get(i);
+        }
+        return res;       
+    }
+}
